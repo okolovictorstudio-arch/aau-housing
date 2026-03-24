38bc564bef08f437e297a17b98017f6cc73dd362
Victor <Okolovictorstudio@gmail.com>
2026-03-24 13:21:55 +0100

    Remove temp admin setup route

--- core/urls.py
+++ core/urls.py
@@ -3,18 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def create_admin(request):
-    if not User.objects.filter(username='aauadmin').exists():
-        User.objects.create_superuser('aauadmin', 'admin@cradle.com', 'Aauhousing2026!')
-        return HttpResponse('Admin created!')
-    return HttpResponse('Admin already exists.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('setup-admin-x7k2/', create_admin),
     path('', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('support/', TemplateView.as_view(template_name='support.html'), name='support'),
